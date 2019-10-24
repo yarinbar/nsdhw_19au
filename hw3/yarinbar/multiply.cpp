fd2c1ea85d812d2139fa29da0937e95c75f7850b
Yarin <yarinbar@campus.technion.ac.il>
2019-10-24 18:50:35 +0800

    matrix

--- hw3/yarinbar/multiply.cpp
+++ hw3/yarinbar/multiply.cpp
@@ -6,12 +6,12 @@ Matrix multiply(const Matrix a, const Matrix b){
     if(a.ncol != b.nrows)
         return nullptr;
 
-    Matrix c(a.nrows, b.ncols);
+    Matrix c(a.nrows(), b.ncols());
 
-    for(int i = 0; i < n; ++i)
-        for(int j = 0; j < n; ++j) {
+    for(int i = 0; i < a.ncol(); ++i)
+        for(int j = 0; j < a.ncol(); ++j) {
             c(i, j) = 0;
-            for(int k = 0; k < n; k++)
+            for(int k = 0; k < a.ncol(); k++)
                 c(i, j) += a(i, k) * b(k, j);
         }
 
