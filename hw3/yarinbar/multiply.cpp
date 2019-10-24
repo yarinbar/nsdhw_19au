7c8594b4842c72c329ca62b61be2bf86a65c2bfa
Yarin <yarinbar@campus.technion.ac.il>
2019-10-24 18:55:51 +0800

    added pointer

--- hw3/yarinbar/multiply.cpp
+++ hw3/yarinbar/multiply.cpp
@@ -1,13 +1,13 @@
 
 #include "matrix.hpp"
 
-Matrix multiply(const Matrix a, const Matrix b){
+Matrix* multiply(const Matrix a, const Matrix b){
 
     // check for dim compatibility
     if(a.ncol() != b.nrow())
         return nullptr;
 
-    Matrix c(a.nrow(), b.ncol());
+    Matrix* c(a.nrow(), b.ncol());
 
     for(unsigned int i = 0; i < a.ncol(); ++i)
         for(unsigned int j = 0; j < a.ncol(); ++j) {
@@ -15,7 +15,7 @@ Matrix multiply(const Matrix a, const Matrix b){
             for(unsigned int k = 0; k < a.ncol(); k++)
                 c(i, j) += a(i, k) * b(k, j);
         }
-
+    return c;
 }
 
 PYBIND11_MODULE(multiply, mod){
